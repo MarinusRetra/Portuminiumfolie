191fe47329c866e5de271368739971cbb4efac89
ThunderBeans (Marinus) <103601175+ThunderBeans@users.noreply.github.com>
2025-09-07 02:23:44 +0200

    setup database connection

--- server/src/server.js
+++ server/src/server.js
@@ -1,20 +1,42 @@
-const cors = require('cors'); const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '../../swag.env') });
-const {Sequelize, DataTypes, Model} = require('sequelize'); 
-const express = require('express'); 
+const cors = require('cors'); 
+const path = require('path'); // Misschien niet nodig.
+require('dotenv').config({ path: path.resolve(__dirname, '../../swag.env') }); //Gebruikt dotenv om key value pairs aan process.env toe te voegen vanuit swag.env
+const {Sequelize, DataTypes, Model} = require('sequelize'); //Definitions voor ORM
+const express = require('express');
 const mysql = require('mysql2'); 
 const app = express(); 
 
-app.use(express.static(path.join(__dirname, "public"))); // Als een aangevraagd bestand in de public map staan dan served het dat bestand.
+class Project extends Model {}
+
+//Middleware voor static static files.
+app.use(express.static(path.join(__dirname, "public"))); // Als een aangevraagd bestand in de public map staat dan geeft het dat bestand.
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Express server is running!');
+//Setup database verbinding met ORM. sequelize is de instance van de database verbinding.
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: 'mysql'
 });
 
-const port = process.env.PORT;
+async function StartServer() {
+  try {
+    await sequelize.authenticate();
+    console.log('Database connected');
+    app.listen(process.env.PORT, () => { console.log(`Server listening`); });
+  } 
+  catch (error) {
+    console.error('Unable to connect', error);
+  }
+}
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+StartServer();
+
+app.get('/', (req, res) => { // Als een request binnenkomt op de root stuur "server is running!" terug
+  res.send('Swag'); //The coolest get request in town.
 });
+
+
+
+
+
